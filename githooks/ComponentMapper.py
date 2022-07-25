c786006b79bd802e0201194f01c9424b5447dada
Vallari Agrawal <val.agl002@gmail.com>
2022-07-25 19:22:11 +0530

    add fnmatch
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- githooks/ComponentMapper.py
+++ githooks/ComponentMapper.py
@@ -1,33 +1,35 @@
 import os, csv
+import fnmatch, re
 from collections import Counter
+
 
 class PathToComponent():
   def __init__(self) -> None:
-    self.map_ = self._get_mapper()
-    
+    self.mapper = self._get_mapper()
+
   def _get_mapper(self):
-    map_ = {}
+    mapper = []
     mapper_path = self._mapper_path
     mapper_file = open(mapper_path, newline='')
     mapper_reader = csv.reader(mapper_file, delimiter=' ', skipinitialspace=True)
 
     for (path, component) in mapper_reader:
-      map_[path] = component
-    return map_
+      path_regex = fnmatch.translate(path)
+      path_regexobj = re.compile(path_regex)
+      mapper += [(component, path_regexobj)]
+    return mapper
 
   @property
   def _mapper_path(self):
     ROOT_GITHOOK_DIR = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(ROOT_GITHOOK_DIR, 'file_component_mapping')
-
-  # def does_component_exist(self, component: str) -> bool:
-    # return component in self.map_
   
   def get_component(self, path: str) -> str:
-    if path in self.map_:
-      return self.map_[path]
+    for (component, path_regexobj) in self.mapper:
+      if path_regexobj.match(path):
+        return component
     file_dir, filename = os.path.split(path)
-    if file_dir in [".", "", "/"]: # file at root of the repo
+    if file_dir in [".", "", "/"]:
       return filename
     return file_dir
 
@@ -37,7 +39,6 @@ class PathToComponent():
       component_ = self.get_component(path)
       if component_:
         components += [component_]
-
     sorted_components = [item[0] for item in Counter(components).most_common()]
     return sorted_components
 
