57a6cb934806a654798e6013740216f3ea95e476
Vallari Agrawal <val.agl002@gmail.com>
2023-11-20 16:40:03 +0530

    WIP: add BasicTests._run_cmd()
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- qa/tasks/nvmeof.py
+++ qa/tasks/nvmeof.py
@@ -10,6 +10,7 @@ from tasks.cephadm import _shell
 
 log = logging.getLogger(__name__)
 
+conf_file = '/etc/ceph/nvmeof.env'
 
 class Nvmeof(Task):
     """
@@ -28,14 +29,12 @@ class Nvmeof(Task):
 
     def setup(self):
         super(Nvmeof, self).setup()
-        log.info('NVMEOF_DEUG: self.config %s' % self.config)
         try:
             self.client = self.config['client']
         except KeyError:
             raise ConfigError('nvmeof requires a client to connect with')
 
         self.cluster_name, type_, self.client_id = teuthology.split_role(self.client)
-        log.info(f'NVMEOF_DEUG: self.cluster_name {self.cluster_name} / type_ {type_} / self.client_id {self.client_id}')
         if type_ != 'client':
             msg = 'client role ({0}) must be a client'.format(self.client)
             raise ConfigError(msg)
@@ -63,10 +62,9 @@ class Nvmeof(Task):
         """
         Deploy nvmeof gateway.
         """
-        log.info('NVMEOF_DEUG: deploy_nvmeof running...')
+        log.info('[nvmeof]: deploying nvmeof gateway...')
         if not hasattr(self.ctx, 'ceph'):
             self.ctx.ceph = {}
-        # cluster_name = self.config.get('cluster', 'ceph')
         fsid = self.ctx.ceph[self.cluster_name].fsid
 
         nodes = []
@@ -83,7 +81,7 @@ class Nvmeof(Task):
         if nodes:
             image = self.nvmeof_image
             if (image != "default"):
-                log.info(f'NVMEOF: ceph config set mgr mgr/cephadm/container_image_nvmeof quay.io/ceph/nvmeof:{image}')
+                log.info(f'[nvmeof]: ceph config set mgr mgr/cephadm/container_image_nvmeof quay.io/ceph/nvmeof:{image}')
                 _shell(self.ctx, self.cluster_name, self.remote, [
                     'ceph', 'config', 'set', 'mgr', 
                     'mgr/cephadm/container_image_nvmeof',
@@ -92,27 +90,24 @@ class Nvmeof(Task):
 
             poolname = self.poolname
             imagename = self.imagename
-            # ceph osd pool create mypool
-            log.info('NVMEOF_DEUG: ceph osd pool create mypool')
+
+            log.info('[nvmeof]: ceph osd pool create mypool')
             _shell(self.ctx, self.cluster_name, self.remote, [
                 'ceph', 'osd', 'pool', 'create', poolname
             ])
 
-            # rbd pool init -p mypool
-            log.info('NVMEOF_DEUG: rbd pool init -p mypool')
+            log.info('[nvmeof]: rbd pool init -p mypool')
             _shell(self.ctx, self.cluster_name, self.remote, [
                 'rbd', 'pool', 'init', poolname
             ])
 
-            # ceph orch apply nvmeof mypool
-            log.info('NVMEOF_DEUG: ceph orch apply nvmeof mypool')
+            log.info('[nvmeof]: ceph orch apply nvmeof mypool')
             _shell(self.ctx, self.cluster_name, self.remote, [
                 'ceph', 'orch', 'apply', 'nvmeof', poolname, 
                 '--placement', str(len(nodes)) + ';' + ';'.join(nodes)
             ])
 
-            # rbd create mypool/myimage --size 8Gi
-            log.info('NVMEOF_DEUG: rbd create mypool/myimage --size 8Gi')
+            log.info('[nvmeof]: rbd create mypool/myimage --size 8Gi')
             _shell(self.ctx, self.cluster_name, self.remote, [
                 'rbd', 'create', f'{poolname}/{imagename}', '--size', '8Gi'
             ])
@@ -127,22 +122,21 @@ class Nvmeof(Task):
                 wait=False,
                 started=True,
             )
+        log.info("[nvmeof]: executed deploy_nvmeof successfully!")
         
     def set_gateway_cfg(self):
-        log.info('NVMEOF_DEUG: starting set_gateway_cfg...')
+        log.info('[nvmeof]: running set_gateway_cfg...')
         gateway_config = self.config.get('gateway_config', {})
         source_host = gateway_config.get('source')
         target_host = gateway_config.get('target')
         if not (source_host and target_host):
             raise ConfigError('gateway_config requires "source" and "target"')
-        (remote,) = self.ctx.cluster.only(source_host).remotes.keys()
+        remote = list(self.ctx.cluster.only(source_host).remotes.keys())[0]
         ip_address = remote.ip_address
         gateway_name = ""
         nvmeof_daemons = self.ctx.daemons.iter_daemons_of_role('nvmeof', cluster=self.cluster_name)
         for daemon in nvmeof_daemons:
             gateway_name = daemon.name()
-        if gateway_name:
-            gateway_name = gateway_name.strip()
         conf_data = dedent(f"""
             NVMEOF_GATEWAY_IP_ADDRESS={ip_address}
             NVMEOF_GATEWAY_NAME={gateway_name}
@@ -155,13 +149,13 @@ class Nvmeof(Task):
             NVMEOF_PORT={self.port}
             NVMEOF_SRPORT={self.srport}
             """)
-        (target_remote,) = self.ctx.cluster.only(target_host).remotes.keys()
+        target_remote = list(self.ctx.cluster.only(target_host).remotes.keys())[0]
         target_remote.write_file(
-            path='/etc/ceph/nvmeof.env',
+            path=conf_file,
             data=conf_data,
             sudo=True
         )
-        log.info("nvmeof.env created successfully!")
+        log.info("[nvmeof]: executed set_gateway_cfg successfully!")
 
 
 class BasicTests(Nvmeof):
@@ -179,79 +173,71 @@ class BasicTests(Nvmeof):
 
     def setup(self):
         super(BasicTests, self).setup()
+    
+    def _run_cmd(self, args):
+        r = self.remote.run(args=['source', conf_file, run.Raw('&&')] + args, 
+                            stdout=StringIO(), stderr=StringIO())
+        stdout = r.stdout.getvalue()
+        stderr = r.stderr.getvalue()
+        return r, stdout, stderr
 
     def begin(self):
-        self.remote.sh('echo $NVMEOF_GATEWAY_IP_ADDRESS')
-        self.remote.run(args=[
-            "echo", run.Raw('$NVMEOF_GATEWAY_IP_ADDRESS')
-        ])
-        self.remote.sh(['source', '/etc/ceph/nvmeof.env', run.Raw('&&'), 'echo', '$NVMEOF_GATEWAY_IP_ADDRESS'])
-        self.remote.sh(['source', '/etc/ceph/nvmeof.env', run.Raw('&&'), 'echo', run.Raw('$NVMEOF_GATEWAY_IP_ADDRESS')])
-        self.remote.run(args=[
-            'source', '/etc/ceph/nvmeof.env', run.Raw('&&'), 'echo', run.Raw('$NVMEOF_GATEWAY_IP_ADDRESS')
-        ])
         self.test_nvmeof_discovery()
         self.test_nvmeof_connect()
+        self.test_nvmeof_disconnect_all()
         self.test_nvmeof_connect_all()
 
     def test_nvmeof_discovery(self):
         DISCOVERY_PORT="8009"
-        log.info(f"NVMEOF_DEUG_TEST: nvme discover -t tcp -a $IP -s {DISCOVERY_PORT}")
-        r = self.remote.run(args=[
+        log.info(f"[nvmeof test]: nvme discover -t tcp -a $IP -s {DISCOVERY_PORT}")
+        _, stdout, stderr = self._run_cmd(args=[
             "sudo", "nvme", "discover", "-t", "tcp", 
             "-a", run.Raw('$NVMEOF_GATEWAY_IP_ADDRESS'), "-s", DISCOVERY_PORT
-        ], stdout=StringIO(), stderr=StringIO())
-        stdout = r.stdout.getvalue()
-        stderr = r.stderr.getvalue()
+        ])
         expected_discovery_stdout = "subtype: nvme subsystem"
         expected_discovery_stderr = ""
         assert expected_discovery_stdout in stdout, f"Expected stdout: {expected_discovery_stdout}, but got: {stdout}"
         assert expected_discovery_stderr == stderr, f"Expected stderr: {expected_discovery_stderr}, but got: {stderr}"
-        log.info(f"NVMEOF_DEUG_TEST: test_nvmeof_discovery successful!")
+        log.info(f"[nvmeof test]: test_nvmeof_discovery successful!")
 
     def test_nvmeof_connect(self):
-        log.info(f"NVMEOF_DEUG_TEST: nvme connect -t tcp --traddr $NVMEOF_GATEWAY_IP_ADDRESS -s $NVMEOF_PORT -n $NVMEOF_NQN")
-        r_ = self.remote.run(args=[
+        log.info(f"[nvmeof test]: nvme connect -t tcp --traddr $NVMEOF_GATEWAY_IP_ADDRESS -s $NVMEOF_PORT -n $NVMEOF_NQN")
+        _, stdout_, stderr_ = self._run_cmd(args=[
             "sudo", "nvme", "connect", "-t", "tcp", 
             "--traddr", run.Raw('$NVMEOF_GATEWAY_IP_ADDRESS'), 
             "-s",  run.Raw('$NVMEOF_PORT'), 
             "-n",  run.Raw('$NVMEOF_NQN')
-        ], stdout=StringIO(), stderr=StringIO())
-        stdout_ = r_.stdout.getvalue()
-        stderr_ = r_.stderr.getvalue()
-        log.info("stderr of connect '%s'" % stderr_)
-        log.info(f"NVMEOF_DEUG_TEST: nvme list")
-        r = self.remote.run(args=["sudo", "nvme", "list"], stdout=StringIO(), stderr=StringIO())
-        stdout = r.stdout.getvalue()
-        stderr = r.stderr.getvalue()
+        ])
+        log.info("stderr_ of connect '%s'" % stderr_)
+        log.info("stdout_ of connect '%s'" % stdout_)
+        log.info(f"[nvmeof test]: nvme list")
+        _, stdout, stderr = self._run_cmd(args=["sudo", "nvme", "list"])
         expected_connect_stdout = "SPDK bdev Controller"
         expected_connect_stderr = ""
         assert expected_connect_stdout in stdout, f"Expected stdout: {expected_connect_stdout}, but got: {stdout}"
         assert expected_connect_stderr == stderr, f"Expected stderr: {expected_connect_stderr}, but got: {stderr}"
-        log.info(f"NVMEOF_DEUG_TEST: test_nvmeof_connect successful!")
+        log.info(f"[nvmeof test]: test_nvmeof_connect successful!")
+
+    def test_nvmeof_disconnect_all(self):
+        log.info(f"[nvmeof test]: nvme disconnect-all")
+        self._run_cmd(args=["sudo", "nvme", "disconnect-all"])
+        _, stdout, _ = self._run_cmd(args=[
+            "sudo", "nvme", "list", run.Raw('|'), "wc", "-l" 
+        ])
+        assert 2 == int(stdout.strip()), f"Expected stdout: 2, but got: {int(stdout.strip())}"
+        log.info(f"[nvmeof test]: test_nvmeof_disconnect_all successful!")
 
     def test_nvmeof_connect_all(self):
-        # nvme connect-all commands after nvme disconnect-all
-        log.info(f"NVMEOF_DEUG_TEST: nvme disconnect-all")
-        self.remote.run(args=["sudo", "nvme", "disconnect-all" ])
-        r_ = self.remote.run(args=[
-            "sudo", "nvme", "list", run.Raw('|'), "wc", "-l" 
-        ], stdout=StringIO())
-        stdout = r_.stdout.getvalue()
-        assert 2 == int(stdout.strip()), f"Expected stdout: 2, but got: {int(stdout.strip())}"
-
-        log.info(f"NVMEOF_DEUG_TEST: nvme connect-all --traddr=$NVMEOF_GATEWAY_IP_ADDRESS --transport=tcp")
-        self.remote.run(args=[
+        log.info(f"[nvmeof test]: nvme connect-all --traddr=$NVMEOF_GATEWAY_IP_ADDRESS --transport=tcp")
+        self._run_cmd(args=[
             "sudo", "nvme", "connect-all", run.Raw('--traddr=$NVMEOF_GATEWAY_IP_ADDRESS'), "--transport=tcp", 
         ])
-        r = self.remote.run(args=["sudo", "nvme", "list"], stdout=StringIO(), stderr=StringIO())
-        stdout = r.stdout.getvalue()
-        stderr = r.stderr.getvalue()
+        _, stdout, stderr = self._run_cmd(args=["sudo", "nvme", "list"])
         expected_connect_stdout = "SPDK bdev Controller"
         expected_connect_stderr = ""
         assert expected_connect_stdout in stdout, f"Expected stdout: {expected_connect_stdout}, but got: {stdout}"
         assert expected_connect_stderr == stderr, f"Expected stderr: {expected_connect_stderr}, but got: {stderr}"
-        log.info(f"NVMEOF_DEUG_TEST: test_nvmeof_connect_all successful!")
+        log.info(f"[nvmeof test]: test_nvmeof_connect_all successful!")
 
 
 task = Nvmeof
