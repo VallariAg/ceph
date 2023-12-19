4e491861cb9eb5cbcc3adef16823761526048b56
Vallari Agrawal <val.agl002@gmail.com>
2023-12-19 12:19:32 +0530

    Create NvmeCommands() class
    and put all fio functions under FIO_Test
    
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- qa/tasks/nvmeof.py
+++ qa/tasks/nvmeof.py
@@ -1,4 +1,5 @@
 import logging
+import json
 from textwrap import dedent
 from io import StringIO
 from teuthology.task import Task
@@ -14,6 +15,7 @@ from teuthology.packaging import install_package, remove_package
 log = logging.getLogger(__name__)
 
 conf_file = '/etc/ceph/nvmeof.env'
+SPDK_CONTROLLER = "SPDK bdev Controller"
 
 class Nvmeof(Task):
     """
@@ -171,6 +173,73 @@ class Nvmeof(Task):
         log.info("[nvmeof]: executed set_gateway_cfg successfully!")
 
 
+class NvmeCommands():
+    def __init__(self, remote) -> None:
+        self.remote = remote
+
+    def _run_cmd(self, args):
+        r = self.remote.run(args=['source', conf_file, run.Raw('&&')] + args, 
+                            stdout=StringIO(), stderr=StringIO())
+        stdout = r.stdout.getvalue()
+        stderr = r.stderr.getvalue()
+        return r, stdout, stderr
+
+    def run_discovery(self):
+        DISCOVERY_PORT="8009"
+        log.info(f"[nvmeof]: nvme discover -t tcp -a $IP -s {DISCOVERY_PORT}")
+        _, stdout, _ = self._run_cmd(args=[
+            "sudo", "nvme", "discover", "-t", "tcp", 
+            "-a", run.Raw('$NVMEOF_GATEWAY_IP_ADDRESS'), "-s", DISCOVERY_PORT
+        ])
+        expected_discovery_stdout = "subtype: nvme subsystem"
+        assert expected_discovery_stdout in stdout, f"Expected stdout: {expected_discovery_stdout}, but got: {stdout}"
+        log.info(f"[nvmeof]: run_discovery successful!")
+
+    def run_connect(self):
+        log.info(f"[nvmeof]: nvme connect -t tcp --traddr $NVMEOF_GATEWAY_IP_ADDRESS -s $NVMEOF_PORT -n $NVMEOF_NQN")
+        self._run_cmd(args=[
+            "sudo", "nvme", "connect", "-t", "tcp", 
+            "--traddr", run.Raw('$NVMEOF_GATEWAY_IP_ADDRESS'), 
+            "-s",  run.Raw('$NVMEOF_PORT'), 
+            "-n",  run.Raw('$NVMEOF_NQN')
+        ])
+        log.info(f"[nvmeof]: nvme list")
+        _, stdout, _ = self._run_cmd(args=["sudo", "nvme", "list"])
+        expected_connect_stdout = SPDK_CONTROLLER
+        assert expected_connect_stdout in stdout, f"Expected stdout: {expected_connect_stdout}, but got: {stdout}"
+        log.info(f"[nvmeof]: run_connect successful!")
+
+    def run_disconnect_all(self):
+        log.info(f"[nvmeof]: nvme disconnect-all")
+        self._run_cmd(args=["sudo", "nvme", "disconnect-all"])
+        _, stdout, _ = self._run_cmd(args=[
+            "sudo", "nvme", "list"
+        ])
+        removed_controller = SPDK_CONTROLLER
+        assert removed_controller not in stdout, f"Expected stdout: no {removed_controller}, but got: {stdout}"
+        log.info(f"[nvmeof]: run_disconnect_all successful!")
+
+    def run_connect_all(self):
+        log.info(f"[nvmeof]: nvme connect-all --traddr=$NVMEOF_GATEWAY_IP_ADDRESS --transport=tcp")
+        self._run_cmd(args=[
+            "sudo", "nvme", "connect-all", run.Raw('--traddr=$NVMEOF_GATEWAY_IP_ADDRESS'), "--transport=tcp", 
+        ])
+        _, stdout, _ = self._run_cmd(args=["sudo", "nvme", "list"])
+        expected_connect_stdout = SPDK_CONTROLLER
+        assert expected_connect_stdout in stdout, f"Expected stdout: {expected_connect_stdout}, but got: {stdout}"
+        log.info(f"[nvmeof]: run_connect_all successful!")
+
+    def run_list_subsys(self, expected_count):
+        log.info(f"[nvmeof]: nvme list-subsys --output-format=json")
+        _, stdout, _ = self._run_cmd(args=[
+            "sudo", "nvme", "list-subsys", "--output-format=json", 
+        ])
+        subsys_output = json.loads(stdout)
+        subsys_list = subsys_output[0]["Subsystems"]
+        assert expected_count == len(subsys_list), f"Expected subsystems: {expected_count}, but got: {len(subsys_list)}"
+        log.info(f"[nvmeof]: run_list_subsys successful!")
+
+
 class BasicTests(Nvmeof):
     """
     Basic nvmeof tests in which following commands are tested:
@@ -178,6 +247,7 @@ class BasicTests(Nvmeof):
     2. connect-all
     3. connect
     4. disconnect-all
+    5. list-subsys
 
         - nvmeof.basic_tests:
             client: client.1
@@ -186,193 +256,121 @@ class BasicTests(Nvmeof):
 
     def setup(self):
         super(BasicTests, self).setup()
-    
-    def _run_cmd(self, args):
-        r = self.remote.run(args=['source', conf_file, run.Raw('&&')] + args, 
-                            stdout=StringIO(), stderr=StringIO())
-        stdout = r.stdout.getvalue()
-        stderr = r.stderr.getvalue()
-        return r, stdout, stderr
 
     def begin(self):
-        self.test_nvmeof_discovery()
-        self.test_nvmeof_connect()
-        self.test_nvmeof_disconnect_all()
-        self.test_nvmeof_connect_all()
-        self.test_device_size()
+        cmd = NvmeCommands(self.remote)
+        cmd.run_discovery()
+        cmd.run_connect()
+        cmd.run_list_subsys(expected_count=2)
+        cmd.run_disconnect_all()
+        cmd.run_list_subsys(expected_count=1)
+        cmd.run_connect_all()
+        self.test_device_size(cmd)
 
-    def test_device_size(self):
-        nvme_model = "SPDK bdev Controller"
-        _, nvme_size_bytes, _ = self._run_cmd(args=[
+    def test_device_size(self, cmd):
+        log.info(f"[nvmeof]: testing device size")
+        nvme_model = SPDK_CONTROLLER
+        _, nvme_size_bytes, _ = cmd._run_cmd(args=[
             "sudo", "nvme", "list", "--output-format=json", run.Raw("|"),
             "jq", "-r", f'.Devices | .[] | select(.ModelNumber == "{nvme_model}") | .PhysicalSize',
         ])
-        _, rbd_image_size_bytes, _ = self._run_cmd(args=[
+        _, rbd_image_size_bytes, _ = cmd._run_cmd(args=[
             'rbd', 'info', '--format=json', run.Raw('$NVMEOF_POOL/$NVMEOF_RBD_IMAGE'), run.Raw("|"),
             "jq", "-r", '.size',
         ])
         assert rbd_image_size_bytes == nvme_size_bytes, f"Expected RBD Image Size: {rbd_image_size_bytes}, nvme size: {nvme_size_bytes}"
-
-    def test_nvmeof_discovery(self):
-        DISCOVERY_PORT="8009"
-        log.info(f"[nvmeof test]: nvme discover -t tcp -a $IP -s {DISCOVERY_PORT}")
-        _, stdout, stderr = self._run_cmd(args=[
-            "sudo", "nvme", "discover", "-t", "tcp", 
-            "-a", run.Raw('$NVMEOF_GATEWAY_IP_ADDRESS'), "-s", DISCOVERY_PORT
-        ])
-        expected_discovery_stdout = "subtype: nvme subsystem"
-        # expected_discovery_stderr = ""
-        assert expected_discovery_stdout in stdout, f"Expected stdout: {expected_discovery_stdout}, but got: {stdout}"
-        # assert expected_discovery_stderr == stderr, f"Expected stderr: {expected_discovery_stderr}, but got: {stderr}"
-        log.info(f"[nvmeof test]: test_nvmeof_discovery successful!")
-
-    def test_nvmeof_connect(self):
-        log.info(f"[nvmeof test]: nvme connect -t tcp --traddr $NVMEOF_GATEWAY_IP_ADDRESS -s $NVMEOF_PORT -n $NVMEOF_NQN")
-        _, stdout_, stderr_ = self._run_cmd(args=[
-            "sudo", "nvme", "connect", "-t", "tcp", 
-            "--traddr", run.Raw('$NVMEOF_GATEWAY_IP_ADDRESS'), 
-            "-s",  run.Raw('$NVMEOF_PORT'), 
-            "-n",  run.Raw('$NVMEOF_NQN')
-        ])
-        log.info(f"[nvmeof test]: nvme list")
-        _, stdout, stderr = self._run_cmd(args=["sudo", "nvme", "list"])
-        expected_connect_stdout = "SPDK bdev Controller"
-        # expected_connect_stderr = ""
-        assert expected_connect_stdout in stdout, f"Expected stdout: {expected_connect_stdout}, but got: {stdout}"
-        # assert expected_connect_stderr == stderr, f"Expected stderr: {expected_connect_stderr}, but got: {stderr}"
-        log.info(f"[nvmeof test]: test_nvmeof_connect successful!")
-
-    def test_nvmeof_disconnect_all(self):
-        log.info(f"[nvmeof test]: nvme disconnect-all")
-        self._run_cmd(args=["sudo", "nvme", "disconnect-all"])
-        _, stdout, _ = self._run_cmd(args=[
-            "sudo", "nvme", "list"
-        ])
-        removed_controller = "SPDK bdev Controller"
-        assert removed_controller not in stdout, f"Expected stdout: no {removed_controller}, but got: {stdout}"
-        log.info(f"[nvmeof test]: test_nvmeof_disconnect_all successful!")
-
-    def test_nvmeof_connect_all(self):
-        log.info(f"[nvmeof test]: nvme connect-all --traddr=$NVMEOF_GATEWAY_IP_ADDRESS --transport=tcp")
-        self._run_cmd(args=[
-            "sudo", "nvme", "connect-all", run.Raw('--traddr=$NVMEOF_GATEWAY_IP_ADDRESS'), "--transport=tcp", 
-        ])
-        _, stdout, stderr = self._run_cmd(args=["sudo", "nvme", "list"])
-        expected_connect_stdout = "SPDK bdev Controller"
-        # expected_connect_stderr = ""
-        assert expected_connect_stdout in stdout, f"Expected stdout: {expected_connect_stdout}, but got: {stdout}"
-        # assert expected_connect_stderr == stderr, f"Expected stderr: {expected_connect_stderr}, but got: {stderr}"
-        log.info(f"[nvmeof test]: test_nvmeof_connect_all successful!")
+        log.info(f"[nvmeof]: test_device_size successful!")
 
 
 class FIO_Test(Nvmeof):
     """
 
-    IO Tests for NVMe-oF
+    IO tests for nvmeof
 
         - nvmeof.fio_test:
-            client.2:
+            client: client.2
+            conf:
                 runtime: 600
     """
     name = 'nvmeof.fio_test'
 
-    def setup(self): # TODO: REWRITE
-        log.info("nvmeof FIO test starting...")
-        pass
-
     def begin(self):
+        cmd = NvmeCommands(self.remote)
+        cmd.run_connect_all()
         rbd_test_dir = teuthology.get_testdir(self.ctx) + "/nvmeof_fio_test"
-        log.info("self.config")
-        log.info(self.config)
-        log.info("self.config.get('all')")
-        log.info(self.config.get('all'))
-        with parallel() as p:
-            if self.config.get('all'):
-                clients = self.ctx.cluster.only(teuthology.is_type('client'))
-                for remote,role in clients.remotes.items():
-                    p.spawn(run_fio, remote, self.config.get('all'), rbd_test_dir)
-            else:
-                for role, client_config in self.config.items():
-                    remote = list(self.ctx.cluster.only(role).remotes.keys())[0]
-                    p.spawn(run_fio, remote, client_config, rbd_test_dir)
+        log.info(self.config.get('conf'))
+        self.run_fio(self.config.get('conf'), rbd_test_dir)
+        cmd.run_disconnect_all()
+
+    def _get_device_path(self):
+        log.info("[nvmeof]: getting drive path..")
+        nvme_model = SPDK_CONTROLLER
+        r = self.remote.run(args=[
+            "sudo", "nvme", "list", "--output-format=json", run.Raw("|"),
+            "jq", "-r", f'.Devices | .[] | select(.ModelNumber == "{nvme_model}") | .DevicePath',
+        ], stdout=StringIO(), stderr=StringIO())
+        nvme_device = r.stdout.getvalue().strip()
+        if nvme_device:
+            return nvme_device
+
+    def _set_config_defaults(self, config):
+        config['ioengine'] = config.get('ioengine', 'libaio')
+        config['bsrange'] = config.get('bsrange', '4k-64k')
+        config['numjobs'] = config.get('numjobs', '1')
+        config['size'] = config.get('size', '1G')
+        config['runtime'] = config.get('runtime', '600') # 10 mins
+        config['rw'] = config.get('rw', 'randrw')
+        config['filename'] = self._get_device_path()
+        return config
 
 
-def _get_drive(remote):
-    log.info("getting nvme drive path..")
-    nvme_model = "SPDK bdev Controller"
-    r = remote.run(args=[
-        "sudo", "nvme", "list", "--output-format=json", run.Raw("|"),
-        "jq", "-r", f'.Devices | .[] | select(.ModelNumber == "{nvme_model}") | .DevicePath',
-    ], stdout=StringIO(), stderr=StringIO())
-    nvme_device = r.stdout.getvalue().strip()
-    log.info("nvme_device")
-    log.info(nvme_device)
-    if nvme_device:
-        return nvme_device
+    def write_fio_config(self, config):
+        """
+        Default:
+        fio --name=nvmeof-fio-test --ioengine=libaio --filename /dev/nvme1n1 --rw=randrw /
+            --bsrange=4k-64k --size=1G --numjobs=4 --time_based --runtime=600 --verify=md5 --verify_fatal=1
+        """
+        fio_config=NamedTemporaryFile(mode='w', prefix='fio_nvmeof_', dir='/tmp/', delete=False)
+        fio_config.write('[nvmeof-fio-test]\n')
+        fio_config.write('ioengine={ioe}\n'.format(ioe=config['ioengine']))
+        fio_config.write('bsrange={bsrange}\n'.format(bsrange=config['bsrange']))
+        fio_config.write('numjobs={numjobs}\n'.format(numjobs=config['numjobs']))
+        fio_config.write('size={size}\n'.format(size=config['size']))
+        fio_config.write('time_based=1\n')
+        fio_config.write('runtime={runtime}\n'.format(runtime=config['runtime']))
+        fio_config.write('rw={rw}\n'.format(rw=config['rw']))
+        fio_config.write('filename={rbd_dev}\n'.format(rbd_dev=config['filename']))
+        fio_config.write('verify=md5\n')
+        fio_config.write('verify_fatal=1\n')
+        fio_config.close()
+        self.remote.put_file(fio_config.name,fio_config.name)
+        return fio_config
 
-def set_config_defaults(config, remote):
-    config['ioengine'] = config.get('ioengine', 'libaio')
-    config['bsrange'] = config.get('bsrange', '4k-64k')
-    # config['bs'] = config.get('io-engine', '4k')
-    config['numjobs'] = config.get('numjobs', '4')
-    config['size'] = config.get('size', '1G')
-    config['runtime'] = config.get('runtime', '600') # 10 mins
-    config['rw'] = config.get('rw', 'randrw')
-    config['filename'] = _get_drive(remote)
-    return config
+    def run_fio(self, config, rbd_test_dir):
+        config = self._set_config_defaults(config)
+        fio_version ='3.35'
+        host_name=self.remote.shortname
+        ioengine_pkg = None
+        try:
+            log.info("[nvmeof]: Running nvmeof fio test on {host_name}".format(host_name=host_name))
+            if config['ioengine'] == 'libaio':
+                system_type = teuthology.get_system_type(self.remote)
+                ioengine_pkg = 'libaio-devel' if system_type == 'rpm' else 'libaio-dev'
+                log.info("[nvmeof]: Installing {ioengine_pkg} on {system_type}".format(ioengine_pkg=ioengine_pkg, system_type=system_type))
+                install_package(ioengine_pkg, self.remote)
+            fio_config = self.write_fio_config(config)
 
-
-def write_fio_config(config, remote):
-    """ # TODO: remove
-[nvmeof-fio-test]
-fio --name=test1 --ioengine=libaio --filename /dev/nvme1n1 --rw=randrw --bsrange=4k-64k --size=1G --numjobs=4 --time_based --runtime=600 --verify=md5 --verify_fatal=1
-    """
-    fio_config=NamedTemporaryFile(mode='w', prefix='fio_nvmeof_', dir='/tmp/', delete=False)
-    fio_config.write('[nvmeof-fio-test]\n')
-    fio_config.write('ioengine={ioe}\n'.format(ioe=config['ioengine']))
-    # fio_config.write('bs={bs}\n'.format(bs=config['bs']))
-    fio_config.write('bsrange={bsrange}\n'.format(bsrange=config['bsrange']))
-    fio_config.write('numjobs={numjobs}\n'.format(numjobs=config['numjobs']))
-    fio_config.write('size={size}\n'.format(size=config['size']))
-    fio_config.write('time_based=1\n')
-    fio_config.write('runtime={runtime}\n'.format(runtime=config['runtime']))
-    fio_config.write('rw={rw}\n'.format(rw=config['rw']))
-    fio_config.write('filename={rbd_dev}\n'.format(rbd_dev=config['filename']))
-    fio_config.write('verify=md5\n')
-    fio_config.write('verify_fatal=1\n')
-    fio_config.close()
-    remote.put_file(fio_config.name,fio_config.name)
-    return fio_config
-
-def run_fio(remote, config, rbd_test_dir):
-    log.info("run_fio config:")
-    log.info(config)
-    config = set_config_defaults(config, remote)
-    log.info("run_fio config after set_config_defaults:")
-    log.info(config)
-    fio_version ='3.35'
-    host_name=remote.shortname
-    ioengine_pkg = None
-    try:
-        log.info("Running nvmeof fio test on {host_name}".format(host_name=host_name))
-        if config['ioengine'] == 'libaio':
-            system_type = teuthology.get_system_type(remote)
-            ioengine_pkg = 'libaio-devel' if system_type == 'rpm' else 'libaio-dev'
-            log.info("Installing {ioengine_pkg} on {system_type}".format(ioengine_pkg=ioengine_pkg, system_type=system_type))
-            install_package(ioengine_pkg, remote)
-        fio_config = write_fio_config(config, remote)
-
-        fio = "https://github.com/axboe/fio/archive/fio-" + fio_version + ".tar.gz"
-        remote.run(args=['mkdir', run.Raw(rbd_test_dir),])
-        remote.run(args=['cd' , run.Raw(rbd_test_dir),
-                         run.Raw(';'), 'wget', fio, run.Raw(';'), run.Raw('tar -xvf fio*tar.gz'), run.Raw(';'),
-                         run.Raw('cd fio-fio*'), run.Raw(';'), './configure', run.Raw(';'), 'make'])
-        remote.run(args=[run.Raw('{tdir}/fio-fio-{v}/fio --showcmd {f}'.format(tdir=rbd_test_dir,v=fio_version,f=fio_config.name))])
-        remote.run(args=['sudo', run.Raw('{tdir}/fio-fio-{v}/fio {f}'.format(tdir=rbd_test_dir,v=fio_version,f=fio_config.name))])
-    finally:
-        remote.run(args=['rm','-rf', run.Raw(rbd_test_dir)])
-        if ioengine_pkg:
-            remove_package(ioengine_pkg, remote)
+            fio = "https://github.com/axboe/fio/archive/fio-" + fio_version + ".tar.gz"
+            self.remote.run(args=['mkdir', run.Raw(rbd_test_dir),])
+            self.remote.run(args=['cd' , run.Raw(rbd_test_dir),
+                             run.Raw(';'), 'wget', fio, run.Raw(';'), run.Raw('tar -xvf fio*tar.gz'), run.Raw(';'),
+                             run.Raw('cd fio-fio*'), run.Raw(';'), './configure', run.Raw(';'), 'make'])
+            self.remote.run(args=[run.Raw('{tdir}/fio-fio-{v}/fio --showcmd {f}'.format(tdir=rbd_test_dir,v=fio_version,f=fio_config.name))])
+            self.remote.run(args=['sudo', run.Raw('{tdir}/fio-fio-{v}/fio {f}'.format(tdir=rbd_test_dir,v=fio_version,f=fio_config.name))])
+        finally:
+            self.remote.run(args=['rm','-rf', run.Raw(rbd_test_dir)])
+            if ioengine_pkg:
+                remove_package(ioengine_pkg, self.remote)
 
 
 task = Nvmeof
