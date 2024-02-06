440e15f8a42516e3d520a163965f6e7921815a2c
Vallari Agrawal <val.agl002@gmail.com>
2024-02-06 12:25:45 +0530

    qa/suite/rbd/nvmeof: Deploy multiple gateways and namespaces
    1. Deploy 2 gateways on different nodes, then check for multi-path.
        To add another gateway, only "roles" need to be changed in job yaml.
    2. Create "n" nvmeof namespaces, configured by 'namespaces_count'
    3. Remove "qa/suites/rbd/nvmeof/cluster" and define "roles" in job yaml
        to configure different node roles for each job.
    4. Add "qa/suites/rbd/nvmeof/workloads/nvmeof-2-gateway-2-initiator.yaml"
        which runs fio in parallel on 2 initiators on different namespaces.
    
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- qa/workunits/rbd/nvmeof_fio_test.sh
+++ qa/workunits/rbd/nvmeof_fio_test.sh
@@ -4,7 +4,20 @@ sudo yum -y install fio
 sudo yum -y install sysstat
 
 fio_file=$(mktemp -t nvmeof-fio-XXXX)
-drives_list=$(sudo nvme list --output-format=json | jq -r '.Devices | .[] | select(.ModelNumber == "SPDK bdev Controller") | .DevicePath')
+all_drives_list=$(sudo nvme list --output-format=json | jq -r '.Devices | .[] | select(.ModelNumber == "SPDK bdev Controller") | .DevicePath')
+
+# When the script is passed two arguments (example: `nvmeof_fio_test.sh 1 3`), 
+# then fio runs on namespaces only in the defined range (which is 1 to 3 here). 
+# So if `nvme list` has 5 namespaces with "SPDK Controller", then fio will 
+# run on first 3 namespaces here.
+if [ "$#" -eq 2 ]; then
+    start=$1
+    end=$2
+    selected_drives=$(echo "${all_drives_list[@]}" | sed -n "${start},${end}p")
+else
+    selected_drives="${all_drives_list[@]}"
+fi
+
 
 RUNTIME=${RUNTIME:-600}
 # IOSTAT_INTERVAL=10
@@ -19,7 +32,7 @@ size=${SIZE:-1G}
 time_based=1
 runtime=$RUNTIME
 rw=${RW:-randrw}
-filename=$(echo "$drives_list" | tr '\n' ':' | sed 's/:$//')
+filename=$(echo "$selected_drives" | tr '\n' ':' | sed 's/:$//')
 verify=md5
 verify_fatal=1
 EOF
@@ -29,7 +42,7 @@ sudo fio $fio_file &
 
 if [ -n "$IOSTAT_INTERVAL" ]; then
     iostat_count=$(( RUNTIME / IOSTAT_INTERVAL ))
-    iostat -d $IOSTAT_INTERVAL $iostat_count -h 
+    iostat -d -p $selected_drives $IOSTAT_INTERVAL $iostat_count -h 
 fi
 wait
 
