829c1eed6d9263dd50fc4a438638850be33771b0
Vallari Agrawal <val.agl002@gmail.com>
2024-02-20 18:02:45 +0530

    WIP: add thrash test & remove qa/suites/rbd/nvmeof/cluster dir
    1. Add nvmeof.thrash task [to be tested after HA is merged]
    2. remove qa/suites/rbd/nvmeof/cluster dir and specify roles
       in each job yaml. This is beacause nvmeof_inititaor needs
       2-gateway-2-initiator setup. And nvmeof_thrash needs
       at least 3-gateway-1-initiator setup.
    
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- qa/tasks/nvmeof.py
+++ qa/tasks/nvmeof.py
@@ -1,10 +1,18 @@
 import logging
+import random
+import time
+import signal
+import gevent
+import contextlib
 from textwrap import dedent
+from gevent.event import Event
+from gevent.greenlet import Greenlet
 from teuthology.task import Task
 from teuthology import misc
 from teuthology.exceptions import ConfigError
 from tasks.util import get_remote_for_role
 from tasks.cephadm import _shell
+from tasks.thrasher import Thrasher
 
 log = logging.getLogger(__name__)
 
@@ -157,6 +165,120 @@ class Nvmeof(Task):
                 sudo=True
             )
         log.info("[nvmeof]: executed set_gateway_cfg successfully!")
+
+
+class NvmeofThrasher(Thrasher, Greenlet):
+    def __init__(self, config, daemons) -> None:
+
+        if config is None:
+            self.config = dict()
+        self.config = config
+        self.daemons = daemons
+        self.logger = log.getChild('[nvmeof.thrasher]')
+        self.stopping = Event()
+
+        """ Random seed """
+        self.random_seed = self.config.get('seed', None)
+        if self.random_seed is None:
+            self.random_seed = int(time.time())
+
+        self.rng = random.Random()
+        self.rng.seed(int(self.random_seed))
+
+        """ Thrashing params """
+        self.randomize = bool(self.config.get('randomize', True))
+        self.min_thrash_delay = float(self.config.get('min_thrash_delay', 60.0))
+        self.max_thrash_delay = float(self.config.get('max_thrash_delay', self.min_thrash_delay + 30.0))
+        self.min_revive_delay = float(self.config.get('min_revive_delay', 0.0))
+        self.max_revive_delay = float(self.config.get('max_revive_delay', self.min_revive_delay + 10.0))
+        # self.daemons = list(self.ctx.daemons.iter_daemons_of_role('nvmeof', cluster=self.cluster_name))
+        # self.thread = gevent.spawn(self.do_thrash)
+
+    def log(self, x):
+        self.logger.info(x)
+
+    def _run(self): # overriding 
+        try:
+            self.do_thrash()
+        except Exception as e:
+            self.set_thrasher_exception(e)
+            self.logger.exception("exception:")
+            # allow successful completion so gevent doesn't see an exception...
+    
+    def stop(self):
+        self.stopping.set()
+
+    def do_thrash(self):
+        self.log('start thrashing')
+        self.log(f'seed: {self.random_seed}, , '\
+                 f'max thrash delay: {self.max_thrash_delay}, min thrash delay: {self.min_thrash_delay} '\
+                 f'max revive delay: {self.max_revive_delay}, min revive delay: {self.min_revive_delay} '\
+                 f'daemons: {len(self.daemons)} '\
+                )
+
+        while not self.stopping.is_set():
+            # delay before thrashing
+            thrash_delay = self.min_thrash_delay
+            if self.randomize:
+                thrash_delay = random.randrange(self.min_thrash_delay, self.max_thrash_delay)
+
+            if thrash_delay > 0.0:
+                self.log(f'waiting for {thrash_delay} secs before thrashing')
+                self.stopping.wait(thrash_delay)
+                if self.stopping.is_set():
+                    continue
+
+            # thrashing
+            max_killable = len(self.daemons) - 1 
+            kill_up_to = self.rng.randrange(1, max_killable)
+            dameons_to_kill = self.rng.sample(self.daemons, kill_up_to)
+            
+            self.log('nvmeof daemons to thrash: {m}'.format(m=len(dameons_to_kill)))
+            for daemon in dameons_to_kill:
+                self.log('kill {label}'.format(label=daemon.id_))
+                # daemon.signal(signal.SIGTERM)
+                daemon.stop()
+                    
+            # delay before reviving
+            revive_delay = self.min_revive_delay
+            if self.randomize:
+                revive_delay = random.randrange(self.min_revive_delay, self.max_revive_delay)
+
+            self.log(f'waiting for {revive_delay} secs before reviving')
+            gevent.sleep(revive_delay)
+
+            # revive after thrashing
+            for daemon in dameons_to_kill:
+                self.log('reviving {label}'.format(label=daemon.id_))
+                daemon.start()
+                
+
+@contextlib.contextmanager
+def thrash(ctx, config):
+    if config is None:
+        config = {}
+    assert isinstance(config, dict), \
+        'nvmeof.thrash task only accepts a dict for configuration'
+
+    cluster = config.get('cluster', 'ceph')
+    daemons = list(ctx.daemons.iter_daemons_of_role('nvmeof', cluster))
+    assert len(daemons) > 1, \
+        'nvmeof.thrash task requires at least 2 nvmeof daemon'
+
+    thrasher = NvmeofThrasher(config, daemons)
+    thrasher.start()
+    ctx.ceph[cluster].thrashers.append(thrasher)
+
+    try:
+        log.debug('Yielding')
+        yield
+    finally:
+        log.info('joining nvmeof.thrash')
+        thrasher.stop()
+        if thrasher.exception is not None:
+            raise RuntimeError('error during thrashing')
+        thrasher.join()
+        log.info('done joining')
 
 
 task = Nvmeof
