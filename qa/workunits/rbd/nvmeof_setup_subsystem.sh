9e1af144c911b77d11960eaf662a3c0e39d58b48
Vallari Agrawal <val.agl002@gmail.com>
2024-03-15 19:48:55 +0530

    debug statements - only run subsystem script and add debugging stuff

--- qa/workunits/rbd/nvmeof_setup_subsystem.sh
+++ qa/workunits/rbd/nvmeof_setup_subsystem.sh
@@ -17,11 +17,23 @@ sudo podman run -it $NVMEOF_CLI_IMAGE --server-address $NVMEOF_DEFAULT_GATEWAY_I
 IFS=',' read -ra gateway_ips <<< "$NVMEOF_GATEWAY_IP_ADDRESSES"
 IFS=',' read -ra gateway_names <<< "$NVMEOF_GATEWAY_NAMES"
 
+list_subsystems () { 
+   for i in "${!gateway_ips[@]}"
+    do
+        ip="${gateway_ips[i]}"
+        sudo podman run -it $NVMEOF_CLI_IMAGE --server-address $ip --server-port $NVMEOF_SRPORT --format json subsystem list
+    done
+}
+
+list_subsystems
+
 # add all subsystems
 for i in $(seq 1 $NVMEOF_SUBSYSTEMS_COUNT); do
     subsystem_nqn="${NVMEOF_SUBSYSTEMS_PREFIX}${i}"
     sudo podman run -it $NVMEOF_CLI_IMAGE --server-address $NVMEOF_DEFAULT_GATEWAY_IP_ADDRESS --server-port $NVMEOF_SRPORT subsystem add --subsystem $subsystem_nqn
 done
+
+list_subsystems
 
 # add all gateway listeners 
 for i in "${!gateway_ips[@]}"
@@ -35,12 +47,15 @@ do
     done
 done
 
+list_subsystems
+
 # add all hosts
 for i in $(seq 1 $NVMEOF_SUBSYSTEMS_COUNT); do
     subsystem_nqn="${NVMEOF_SUBSYSTEMS_PREFIX}${i}"
     sudo podman run -it $NVMEOF_CLI_IMAGE --server-address $NVMEOF_DEFAULT_GATEWAY_IP_ADDRESS --server-port $NVMEOF_SRPORT host add --subsystem $subsystem_nqn --host "*"
 done
 
+list_subsystems
 
 # add all namespaces
 image_index=1
@@ -53,13 +68,30 @@ for i in $(seq 1 $NVMEOF_SUBSYSTEMS_COUNT); do
     done
 done
 
+list_subsystems
 # list subsystems
-for i in "${!gateway_ips[@]}"
-do
-    ip="${gateway_ips[i]}"
-    sudo podman run -it $NVMEOF_CLI_IMAGE --server-address $ip --server-port $NVMEOF_SRPORT --format json subsystem list
-done
+# for i in "${!gateway_ips[@]}"
+# do
+#     ip="${gateway_ips[i]}"
+#     sudo podman run -it $NVMEOF_CLI_IMAGE --server-address $ip --server-port $NVMEOF_SRPORT --format json subsystem list
+# done
 
 rados -p mypool listomapvals $(rados -p mypool ls | grep nvmeof)
+
+
+sudo modprobe nvme-fabrics
+sudo modprobe nvme-tcp
+sudo dnf install nvme-cli -y
+sudo lsmod | grep nvme
+
+DISCOVERY_PORT="8009"
+sudo nvme discover -t tcp -a $NVMEOF_DEFAULT_GATEWAY_IP_ADDRESS -s $DISCOVERY_PORT
+sudo nvme connect-all -t tcp --traddr $NVMEOF_DEFAULT_GATEWAY_IP_ADDRESS -l 1800
+nvme list
+sudo nvme list-subsys
+sudo nvme list-subsys --output-format=json
+dmesg -T | tail -n 200
+
+list_subsystems
 
 echo "[nvmeof] Subsystem setup done"
