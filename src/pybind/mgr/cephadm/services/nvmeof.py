487e5ceef9c24f1b93aa914ace8093134396461e
Vallari Agrawal <vallari.agrawal@ibm.com>
2025-06-12 12:19:44 +0530

    mgr/cephadm: add default_listeners spec
    Signed-off-by: Vallari Agrawal <vallari.agrawal@ibm.com>

--- src/pybind/mgr/cephadm/services/nvmeof.py
+++ src/pybind/mgr/cephadm/services/nvmeof.py
@@ -2,7 +2,7 @@ import errno
 import logging
 import json
 from typing import List, cast, Optional
-from ipaddress import ip_address, IPv6Address
+from ipaddress import ip_address, IPv6Address, ip_network
 
 from mgr_module import HandleCommandResult
 from ceph.deployment.service_spec import NvmeofServiceSpec
@@ -65,6 +65,18 @@ class NvmeofService(CephService):
         self.mgr.log.info(f"gateway address: {addr} from {map_addr=} {spec.addr=} {host_ip=}")
         discovery_addr = map_discovery_addr or spec.discovery_addr or host_ip
         self.mgr.log.info(f"discovery address: {discovery_addr} from {map_discovery_addr=} {spec.discovery_addr=} {host_ip=}")
+ 
+        default_listeners = spec.default_listeners # "172.17.0.0/16:3000"
+        if default_listeners:
+            listeners_ip = ""
+            for listeners_subnets in default_listeners.split(','):
+                subnet, port = listeners_subnets.rsplit(':', 1)
+                ip = self.subnet_to_ip(daemon_spec.host, subnet)
+                if ip:
+                    # verify ip exists
+                    if utils.resolve_ip(ip):
+                        listeners_ip += ip + ":" + port
+            default_listeners = listeners_ip
         context = {
             'spec': spec,
             'name': name,
@@ -76,7 +88,8 @@ class NvmeofService(CephService):
             'rpc_socket_name': 'spdk.sock',
             'transport_tcp_options': transport_tcp_options,
             'iobuf_options': iobuf_options,
-            'rados_id': rados_id
+            'rados_id': rados_id,
+            'default_listeners': default_listeners,
         }
         gw_conf = self.mgr.template.render('services/nvmeof/ceph-nvmeof.conf.j2', context)
 
@@ -120,6 +133,17 @@ class NvmeofService(CephService):
         daemon_spec.final_config, daemon_spec.deps = self.generate_config(daemon_spec)
         daemon_spec.deps = []
         return daemon_spec
+
+    def subnet_to_ip(self, host: str, subnet: str) -> Optional[str]:
+        ip_subnet = ip_network(subnet)
+        networks = self.mgr.cache.networks.get(host, {})
+        ipaddr = None
+        for n_subnet, n_ifaces in networks.items(): 
+            if ip_network(n_subnet) == ip_subnet and n_ifaces:
+                # n_ifaces = {'iface': ['ipaddr'], }
+                iface = list(n_ifaces.keys())[0]
+                ipaddr = n_ifaces[iface][0] if n_ifaces[iface] else None
+        return ipaddr
 
     def daemon_check_post(self, daemon_descrs: List[DaemonDescription]) -> None:
         """ Overrides the daemon_check_post to add nvmeof gateways safely
