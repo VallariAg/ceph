de7fcb4625468bd35b9b082dfe7ee48e08d4603a
Vallari Agrawal <vallari.agrawal@ibm.com>
2026-02-10 02:58:28 +0530

    src/pybind/mgr: Add nvmeof-top tool
    Add src/pybind/mgr/dashboard/services/nvmeof_top_cli.py
    which adds commands:
    "ceph nvmeof top io <subsystem>"
    "ceph nvmeof top cpu"
    
    This file is moved from the original work in:
    https://github.com/pcuzner/ceph-nvmeof-top
    
    Co-authored-by: Paul Cuzner <pcuzner@ibm.com>
    Signed-off-by: Vallari Agrawal <vallari.agrawal@ibm.com>

--- src/pybind/mgr/dashboard/services/nvmeof_client.py
+++ src/pybind/mgr/dashboard/services/nvmeof_client.py
@@ -62,6 +62,7 @@ else:
                     None
                 )
                 if matched_gateway:
+                    self.daemon_name = matched_gateway.get('daemon_name')
                     self.gateway_addr = matched_gateway.get('service_url')
                     logger.debug("Gateway address set to: %s", self.gateway_addr)
             enable_auth = is_mtls_enabled(service_name)
@@ -80,6 +81,7 @@ else:
                 logger.info("Insecurely connecting to: %s", self.gateway_addr)
                 self.channel = grpc.insecure_channel(self.gateway_addr)
             self.stub = pb2_grpc.GatewayStub(self.channel)
+            self.service_name = service_name
 
     Model = Dict[str, Any]
     Collection = List[Model]
