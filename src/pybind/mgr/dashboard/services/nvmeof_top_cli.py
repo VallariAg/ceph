5c4a2f530cc073b2c67e3082675f19a861ab9713
Vallari Agrawal <vallari.agrawal@ibm.com>
2026-02-22 21:39:37 +0530

    wip
    Signed-off-by: Vallari Agrawal <vallari.agrawal@ibm.com>

--- src/pybind/mgr/dashboard/services/nvmeof_top_cli.py
+++ src/pybind/mgr/dashboard/services/nvmeof_top_cli.py
@@ -7,12 +7,15 @@ import time
 import logging
 import grpc
 import asyncio
+import json
 
 from .. import mgr
-from mgr_module import CLIReadCommand, HandleCommandResult, CLICommand
+from mgr_module import HandleCommandResult
+from ..cli import DBCLICommand
 
 from .nvmeof_client import NVMeoFClient
 from .nvmeof_cli import NvmeofGatewaysConfig
+from .nvmeof_conf import get_pool_group_name
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -21,6 +24,36 @@ logger.setLevel(logging.DEBUG)
 def get_collector(session_id: str):
     MAX_SESSION_TTL = 60 * 60
     return mgr.get_nvmeof_collector(session_id, MAX_SESSION_TTL)
+
+def get_lbg_gws_map(service_name: str): # TODO: uncomment
+    # pool_group = ['mypool', 'mygroup']
+    pool_group = get_pool_group_name(service_name)
+    if not pool_group:
+        logger.error("Error getting pool name and group name of the service")
+        return {}
+    logger.info(f"VALLARI_DEBUG: {pool_group=}")
+    pool, group = pool_group
+    try:
+        cmd = {
+            'prefix': 'nvme-gw show', 
+            'pool': pool, 
+            'group': group, 
+            'format': 'json'
+        }
+        ret_status, out, _ = mgr.mon_command(cmd)
+        logger.info(f"VALLARI_DEBUG: {ret_status=} {out=}")
+        if ret_status == 0 and out is not None:
+            lbg_gws_map = {}
+            gws_info = json.loads(out)
+            for gw in gws_info["Created Gateways:"]:
+                gw_id = str(gw["gw-id"]).removeprefix("client.")
+                gw_lbg = int(gw["anagrp-id"])
+                lbg_gws_map[gw_lbg] = gw_id
+            return lbg_gws_map
+        return {}
+    except Exception:
+        logger.exception('Failed to get nvme-gw show command')
+        return {}
 
 class NVMeoFTopTool:
     headers = []
@@ -95,8 +128,9 @@ class NVMeoFTopCPU(NVMeoFTopTool):
         return ''.join(rows)  
 
 class NVMeoFTopIO(NVMeoFTopTool):
-    subsystem_summary_headers = ['Subsystem', 'Namespaces', 'Total IOPS', 'Throughput']
-    summary_headers = ['Gateway', 'Total Subsystems', 'Total Namespaces']
+    subsystem_summary_headers = ['Subsystem', 'Namespaces',]
+                                #   'Total IOPS', 'Throughput']
+    summary_headers = ['Gateway', 'Load Balancing Group', 'Total Subsystems', 'Total Namespaces']
 
     ns_headers = ['NSID', 'RBD Image', 'IOPS', 'r/s', 'rMB/s', 'r_await', 'rareq-sz', 'w/s', 'wMB/s', 'w_await', 'wareq-sz', 'LBGrp', 'QoS']
     ns_template = "{:>4}   {:<40}   {:>7}   {:>6}   {:>6}   {:>7}   {:>8}   {:>6}   {:>6}   {:>7}   {:>8}   {:^5}   {:>3}\n"
@@ -140,7 +174,7 @@ class NVMeoFTopIO(NVMeoFTopTool):
         return ''.join(rows) 
 
 
-@CLIReadCommand('nvmeof top cpu', poll=True)
+@DBCLICommand.Read('nvmeof top cpu', poll=True)
 def nvmeof_top_cpu(_, service: str = '',
                 server_addr: str = '', group: str = '',
                 descending: bool = False, sort_by: str = 'Thread Name',
@@ -177,7 +211,7 @@ def nvmeof_top_cpu(_, service: str = '',
         logger.exception(exc)
         return HandleCommandResult(stderr=str(exc), retval=8)
 
-@CLIReadCommand('nvmeof top io', poll=True)
+@DBCLICommand.Read('nvmeof top io', poll=True)
 def nvmeof_top_io(_, subsystem: str = '',
                 server_addr: str = '', group: str = '',
                 descending: bool = False, sort_by: str = 'NSID',
@@ -208,7 +242,7 @@ def nvmeof_top_io(_, subsystem: str = '',
     }
     try:
         if not subsystem:
-            err_message = "Required argument 'subsystem' missing"
+            err_message = "Required argument '--subsystem' missing"
             return HandleCommandResult(stderr=err_message, retval=8)
         gateway_client = NVMeoFClient(group, server_addr)
         data_collector = get_collector(session_id)
@@ -313,6 +347,7 @@ class NvmeofTopCollector:
         self.server_addr = ''
         self.delay = 1000
         self.namespaces = {}
+        self.lbg_gw = {}
         self.subsystems = None
         self.reactor_stats = {}
         self.iostats = {}
@@ -333,6 +368,10 @@ class NvmeofTopCollector:
     def total_namespaces_defined(self) -> int:
         return len(self.namespaces[self.subsystem_nqn])
 
+    # @property
+    # def namespaces_bdevs(self) -> int:
+    #     return [ns.bdev_name for ns in self.namespaces[self.subsystem_nqn]]
+
     @property
     def total_subsystems(self) -> int:
         return len(self.nqn_list)
@@ -344,13 +383,13 @@ class NvmeofTopCollector:
             total += subsys.namespace_count
         return total
 
-    @property
-    def total_iops(self):
-        return int(sum([stats.total_ops_rate for _, stats in self.iostats.items()]))
+    # @property
+    # def total_iops(self):
+    #     return int(sum([stats.total_ops_rate for _, stats in self.iostats[self.server_addr].items()]))
 
-    @property
-    def total_bandwidth(self):
-        return sum([stats.total_bytes_rate for _, stats in self.iostats.items()])
+    # @property
+    # def total_bandwidth(self):
+    #     return sum([stats.total_bytes_rate for _, stats in self.iostats[self.server_addr].items()])
 
     @property
     def max_namespaces(self):
@@ -359,6 +398,11 @@ class NvmeofTopCollector:
                 return subsys.max_namespaces
         logger.error("Request for max namespaces could not find a match against the NQN! Returning 0")
         return 0
+    
+    @property
+    def load_balancing_group(self):
+        logger.info(f"VALLARI_DEBUG: {self.gw_info=}")
+        return self.gw_info.load_balancing_group
 
     def log_connection(self):
         logger.info(f"Connected to {self.server_addr}")
@@ -371,7 +415,12 @@ class NvmeofTopCollector:
             ns_info = f"{ns.rbd_pool_name}/{ns.rbd_image_name}"
             bdev_name = ns.bdev_name
 
-            perf_stats = self.iostats[bdev_name]
+            if not self.cmd_handler.args.get('server_addr'):
+                lbg = ns.load_balancing_group
+                daemon_name = self.lbg_gw[lbg]
+                perf_stats = self.iostats[daemon_name][bdev_name]
+            else:
+                perf_stats = self.iostats[self.client.daemon_name][bdev_name]
             perf_stats.calculate(self.delay)
 
             ns_data.append((
@@ -413,13 +462,14 @@ class NvmeofTopCollector:
         return [
             self.subsystem_nqn, 
             f'{self.total_namespaces_defined} / {self.max_namespaces}', 
-            self.total_iops, 
-            f'{(self.total_bandwidth / 1024**2):>7.2f} MiB/s'
+            # self.total_iops, 
+            # f'{(self.total_bandwidth / 1024**2):>7.2f} MiB/s'
         ]
 
     def get_overall_summary_data(self):
         return [
             self.server_addr,
+            self.load_balancing_group,
             self.total_subsystems,
             self.total_namespaces_overall,
         ]
@@ -454,6 +504,42 @@ class NvmeofTopCollector:
         self.health.msg = f"{method_name} success"
         logger.debug(f"call to {method_name} successful")
         return data
+
+    def _get_namespaces_iostat(self, client):
+        # gateway_addr = client.gateway_addr
+        daemon_name = client.daemon_name
+        
+        logger.info(f"fetching iostats for namespaces from {daemon_name}")
+        with self.iostats_lock:
+            logger.info('iostats lock acquired')
+            # if ns.bdev_name not in self.iostats:
+            #     self.iostats[ns.bdev_name] = PerformanceStats(ns.bdev_name, self.delay)
+            logger.info('calling list_namespaces_io_stats')
+            stats = self.call_grpc_api('list_namespaces_io_stats',
+                                       NVMeoFClient.pb2.list_namespaces_io_stats_req())
+            if daemon_name not in self.iostats:
+                self.iostats[daemon_name] = {}
+            
+            logger.info(stats)
+
+            for ns in stats.namespaces:
+                bdev_name = ns.bdev_name
+                logger.info(f"VALLARI_DEBUG_SHOW {bdev_name}: {ns}")
+                if bdev_name not in self.iostats[daemon_name]:
+                    logger.info(f"VALLARI_DEBUG_SHOW: add PerformanceStats")
+                    self.iostats[daemon_name][bdev_name] = PerformanceStats(bdev_name, self.delay)                
+
+                ns = dict(ns)
+                logger.info(f"VALLARI_DEBUG_SHOW: {ns=}")
+                
+                iostats = self.iostats[daemon_name][bdev_name]
+                iostats.read_ops.update(ns.get('num_read_ops', 0))
+                iostats.read_bytes.update(ns.get('bytes_read', 0))
+                iostats.read_secs.update((ns.get('read_latency_ticks', 0) / stats.tick_rate))
+                iostats.write_ops.update(ns.get('num_write_ops', 0))
+                iostats.write_bytes.update(ns.get('bytes_written', 0))
+                iostats.write_secs.update((ns.get('write_latency_ticks', 0) / stats.tick_rate))
+            logger.info(f"VALLARI_DEBUG_SHOW: {self.iostats}")
 
     def _get_ns_iostats(self, ns):
         logger.debug(f"fetching iostats for namespace {ns.nsid}")
@@ -503,8 +589,8 @@ class NvmeofTopCollector:
     def _get_subsystems(self):
         return self.call_grpc_api('list_subsystems', NVMeoFClient.pb2.list_subsystems_req(subsystem_nqn=self.subsystem_nqn))
     
-    def _get_gateway(self):
-        return self.call_grpc_api('get_gateway_info', NVMeoFClient.pb2.get_gateway_info_req())
+    def _get_gateway(self, client):
+        return self.call_grpc_api('get_gateway_info', NVMeoFClient.pb2.get_gateway_info_req(), client)
 
     def _get_all_subsystems(self):
         return self.call_grpc_api('list_subsystems', NVMeoFClient.pb2.list_subsystems_req())
@@ -519,7 +605,7 @@ class NvmeofTopCollector:
         self.delay = (now - self.timestamp)
         self.timestamp = now
 
-        self._get_gateway()
+        self.gw_info = self._get_gateway(self.client)
         if self.health.rc > 0:
             logger.error(f"Call to {self.server_addr} failed, RC={self.health.rc}, MSG={self.health.msg}")
             self.health.rc = 8
@@ -578,9 +664,29 @@ class NvmeofTopCollector:
         logger.debug(f"Subsystem '{self.subsystem_nqn}' has {self.total_namespaces_defined} namespaces")
 
         tasks = []
-        for ns in self.namespaces[self.subsystem_nqn]:
-            t = asyncio.create_task(asyncio.to_thread(self._get_ns_iostats, ns))
-            tasks.append(t)
+        # LBG_MAP = lbg: server_addr
+        # io_stats = server_addr: bdev: stats
+        # namespaces 
+        # for ns in self.namespaces[self.subsystem_nqn]:
+        #     t = asyncio.create_task(asyncio.to_thread(self._get_ns_iostats, ns))
+        #     tasks.append(t)
+        group = self.cmd_handler.group
+        if not self.cmd_handler.args.get('server_addr'):
+            service_name = self.client.service_name
+            gw_conf = NvmeofGatewaysConfig.get_gateways_config()
+            if service_name not in gw_conf["gateways"]:
+                self.health.rc = 8
+                self.health.msg = f'Service {service_name} not found'
+                return
+            for gw in gw_conf["gateways"][service_name]:
+                client = NVMeoFClient(group, gw["service_url"])
+                self.lbg_gw = get_lbg_gws_map(service_name)
+                logger.info(f"VALLARI_DEBUG: {self.lbg_gw}")
+                r = asyncio.create_task(asyncio.to_thread(self._get_namespaces_iostat, client))
+                tasks.append(r)
+        else:
+            r = asyncio.create_task(asyncio.to_thread(self._get_namespaces_iostat, self.client))
+            tasks.append(r)
 
         await asyncio.gather(*tasks)
         logger.debug("collect_io_data tasks completed")
